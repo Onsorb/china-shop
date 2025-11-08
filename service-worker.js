82685169de7e341cdef055bacb495049b9fa30bf
Ali <aabdalzadeh@gmail.com>
2025-11-08 23:07:24 +0330

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+// service-worker.js
+const CACHE_NAME = 'china-shop-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://cdn.fontcdn.ir/Font/Persian/Vazir/Vazir.css'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
